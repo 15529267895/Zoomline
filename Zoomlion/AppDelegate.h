8a3207038cbed732cbf6afd4749af4b30388e227
王莉 <li.wang@utsoft.cn>
2017-11-12 08:32:24 +0800

    Initial Commit

---
+++ Zoomlion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zoomlion
+//
+//  Created by 王li on 2017/11/12.
+//  Copyright © 2017年 wli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zoomlion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zoomlion
+//
+//  Created by 王li on 2017/11/12.
+//  Copyright © 2017年 wli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
