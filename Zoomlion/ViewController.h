f9fae70d291b062d452606db3c4b2b84084a147b
旺仔 <1728733706@qq.com>
2019-10-29 17:26:16 +0800

    viewcontroller
    增加viewcontroller界面

---
+++ Zoomlion/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Zoomlion
+//
+//  Created by 王li on 2019/10/29.
+//  Copyright © 2019年 wli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Zoomlion/ViewController.m
@@ -0,0 +1,33 @@
+
+//
+//  ViewController.m
+//  Zoomlion
+//
+//  Created by 王li on 2019/10/29.
+//  Copyright © 2019年 wli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
